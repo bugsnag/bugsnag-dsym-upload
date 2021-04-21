ff67066c8412ac4326678398ebe9f8a671894e96
Xander Jones <xander@bugsnag.com>
2021-04-21 15:14:52 +0100

    Revert "`Bundler.with_clean_env` has been deprecated..."
    This reverts commit 61f17a4bd0d2273b39db6fdd46b83034d0e633be.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,10 +1,10 @@
-Bundler.with_unbundled_env do
+Bundler.with_clean_env do
   Dir.chdir 'tools/fastlane-plugin' do
     `rake build`
   end
 end
 
-Bundler.with_unbundled_env do
+Bundler.with_clean_env do
   Dir.chdir 'features/fixtures/fl-project' do
     gem_path = Dir['../../../tools/fastlane-plugin/fastlane-plugin-bugsnag-*.gem'].last
     `bundle config --local path vendor`
@@ -18,7 +18,7 @@ def fastlane_upload_symbols(lane, dsym_path=nil, api_key=nil, config_file=nil)
   config_file_env = "BUGSNAG_CONFIG_FILE='#{config_file}'" unless config_file.nil?
   dsym_path_env = "BUGSNAG_DSYM_PATH='#{dsym_path}'" unless dsym_path.nil?
 
-  Bundler.with_unbundled_env do
+  Bundler.with_clean_env do
     Dir.chdir 'features/fixtures/fl-project' do
       `BUGSNAG_ENDPOINT='http://localhost:#{MOCK_API_PORT}'\
        #{dsym_path_env} \
