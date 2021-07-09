f574b62689b1bed4d7c95f6f88d47ad00695ed3e
Xander Jones <xander@bugsnag.com>
2021-07-09 16:44:27 +0100

    add testing of .xcarchive with special characters.

--- features/steps/upload_steps.rb
+++ features/steps/upload_steps.rb
@@ -2,3 +2,10 @@ When("I upload dSYMS with options {string}") do |args|
   set_script_env('UPLOAD_ARGS', args)
   step('I run the script "features/scripts/upload-with-args.sh" synchronously')
 end
+
+# A scenario where a user wraps the dSYM path in quotes to escape whitespace in the path
+When("I upload dSYMS with options {string} and dSYM path {string}") do |args, dsyms_path|
+  set_script_env('UPLOAD_ARGS', args)
+  set_script_env('DSYMS_PATH', dsyms_path)
+  step('I run the script "features/scripts/upload-with-dsym-path.sh" synchronously')
+end
