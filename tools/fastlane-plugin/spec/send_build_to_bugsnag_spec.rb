3f388aab052d730327db8e1f878650db09f7b8e7
Josh <j.edney17@gmail.com>
2025-09-08 09:22:05 +0100

    update tests for CI

--- tools/fastlane-plugin/spec/send_build_to_bugsnag_spec.rb
+++ tools/fastlane-plugin/spec/send_build_to_bugsnag_spec.rb
@@ -6,6 +6,8 @@ require_relative '../lib/fastlane/plugin/bugsnag/actions/bugsnag_cli'
 
 BuildAction = Fastlane::Actions::SendBuildToBugsnagAction
 BUGSNAG_CLI_PATH = BugsnagCli.get_bundled_path
+GIT_REVISION = `git rev-parse HEAD`.strip
+BUILDER = `whoami`.strip
 
 
 describe BuildAction do
@@ -15,7 +17,7 @@ describe BuildAction do
 
   context 'building an iOS project' do
     it 'detects default Info.plist file excluding test dirs' do
-      expect(Kernel).to receive(:system).with("#{BUGSNAG_CLI_PATH} create-build --api-key 12345678901234567890123456789AAA --version-name 2.0-other --bundle-version 22 --builder-name josh.edney --revision 3a30a510ba898341ff8631da49dc7021fb28c40e --repository git@github.com:bugsnag/bugsnag-dsym-upload.git").and_return(true)
+      expect(Kernel).to receive(:system).with("#{BUGSNAG_CLI_PATH} create-build --api-key 12345678901234567890123456789AAA --version-name 2.0-other --bundle-version 22 --builder-name #{BUILDER} --revision #{GIT_REVISION} --repository https://github.com/bugsnag/bugsnag-dsym-upload").and_return(true)
       Dir.chdir(File.join(FIXTURE_PATH, 'ios_proj')) do
         run_with({})
       end
@@ -24,7 +26,7 @@ describe BuildAction do
     it 'reads api key from legacy location' do
       # the API key is now in `bugsnag.apiKey`, it used to be in 'BugsnagAPIKey',
       # test this can be extracted correctly from the `ios_proj_legacy`
-      expect(Kernel).to receive(:system).with("#{BUGSNAG_CLI_PATH} create-build --api-key 12345678901234567890123456789BBB --version-name 4.0-project --bundle-version 44 --builder-name josh.edney --revision 3a30a510ba898341ff8631da49dc7021fb28c40e --repository git@github.com:bugsnag/bugsnag-dsym-upload.git").and_return(true)
+      expect(Kernel).to receive(:system).with("#{BUGSNAG_CLI_PATH} create-build --api-key 12345678901234567890123456789BBB --version-name 4.0-project --bundle-version 44 --builder-name #{BUILDER} --revision #{GIT_REVISION} --repository https://github.com/bugsnag/bugsnag-dsym-upload").and_return(true)
       Dir.chdir(File.join(FIXTURE_PATH, 'ios_proj_legacy')) do
         run_with({})
       end
@@ -33,7 +35,7 @@ describe BuildAction do
     context 'using default config_file option' do
       context 'override API key from config' do
         it 'reads API key from the api_key option' do
-          expect(Kernel).to receive(:system).with("#{BUGSNAG_CLI_PATH} create-build --api-key 12345678901234567890123456789FFF --version-name 2.0-other --bundle-version 22 --builder-name josh.edney --revision 3a30a510ba898341ff8631da49dc7021fb28c40e --repository git@github.com:bugsnag/bugsnag-dsym-upload.git").and_return(true)
+          expect(Kernel).to receive(:system).with("#{BUGSNAG_CLI_PATH} create-build --api-key 12345678901234567890123456789FFF --version-name 2.0-other --bundle-version 22 --builder-name #{BUILDER} --revision #{GIT_REVISION} --repository https://github.com/bugsnag/bugsnag-dsym-upload").and_return(true)
           Dir.chdir(File.join(FIXTURE_PATH, 'ios_proj')) do
             run_with({
               api_key: '12345678901234567890123456789FFF'
@@ -42,7 +44,7 @@ describe BuildAction do
         end
 
         it 'uses input versions from options' do
-          expect(Kernel).to receive(:system).with("#{BUGSNAG_CLI_PATH} create-build --api-key 12345678901234567890123456789AAA --version-name 8.0.0 --bundle-version 800 --builder-name josh.edney --revision 3a30a510ba898341ff8631da49dc7021fb28c40e --repository git@github.com:bugsnag/bugsnag-dsym-upload.git").and_return(true)
+          expect(Kernel).to receive(:system).with("#{BUGSNAG_CLI_PATH} create-build --api-key 12345678901234567890123456789AAA --version-name 8.0.0 --bundle-version 800 --builder-name #{BUILDER} --revision #{GIT_REVISION} --repository https://github.com/bugsnag/bugsnag-dsym-upload").and_return(true)
           Dir.chdir(File.join(FIXTURE_PATH, 'ios_proj')) do
             run_with({
               app_version: '8.0.0',
@@ -55,7 +57,7 @@ describe BuildAction do
 
     context 'override config_file option' do
       it 'reads API key and version info from the config file' do
-        expect(Kernel).to receive(:system).with("#{BUGSNAG_CLI_PATH} create-build --api-key 12345678901234567890123456789DDD --version-name 3.0-project --bundle-version 33 --builder-name josh.edney --revision 3a30a510ba898341ff8631da49dc7021fb28c40e --repository git@github.com:bugsnag/bugsnag-dsym-upload.git").and_return(true)
+        expect(Kernel).to receive(:system).with("#{BUGSNAG_CLI_PATH} create-build --api-key 12345678901234567890123456789DDD --version-name 3.0-project --bundle-version 33 --builder-name #{BUILDER} --revision #{GIT_REVISION} --repository https://github.com/bugsnag/bugsnag-dsym-upload").and_return(true)
         Dir.chdir(File.join(FIXTURE_PATH, 'ios_proj')) do
           run_with({
             config_file: File.join('Project', 'Info.plist')
@@ -65,7 +67,7 @@ describe BuildAction do
 
       context 'override API key, and config file' do
         it 'uses the input api_key to override a non default config' do
-          expect(Kernel).to receive(:system).with("#{BUGSNAG_CLI_PATH} create-build --api-key 12345678901234567890123456789EEE --version-name 3.0-project --bundle-version 33 --builder-name josh.edney --revision 3a30a510ba898341ff8631da49dc7021fb28c40e --repository git@github.com:bugsnag/bugsnag-dsym-upload.git").and_return(true)
+          expect(Kernel).to receive(:system).with("#{BUGSNAG_CLI_PATH} create-build --api-key 12345678901234567890123456789EEE --version-name 3.0-project --bundle-version 33 --builder-name #{BUILDER} --revision #{GIT_REVISION} --repository https://github.com/bugsnag/bugsnag-dsym-upload").and_return(true)
           Dir.chdir(File.join(FIXTURE_PATH, 'ios_proj')) do
             run_with({
               config_file: File.join('Project', 'Info.plist'),
@@ -75,7 +77,7 @@ describe BuildAction do
         end
 
         it 'uses the input versions to override a non default config' do
-          expect(Kernel).to receive(:system).with("#{BUGSNAG_CLI_PATH} create-build --api-key 12345678901234567890123456789DDD --version-name 9.0.0 --bundle-version 900 --builder-name josh.edney --revision 3a30a510ba898341ff8631da49dc7021fb28c40e --repository git@github.com:bugsnag/bugsnag-dsym-upload.git").and_return(true)
+          expect(Kernel).to receive(:system).with("#{BUGSNAG_CLI_PATH} create-build --api-key 12345678901234567890123456789DDD --version-name 9.0.0 --bundle-version 900 --builder-name #{BUILDER} --revision #{GIT_REVISION} --repository https://github.com/bugsnag/bugsnag-dsym-upload").and_return(true)
           Dir.chdir(File.join(FIXTURE_PATH, 'ios_proj')) do
             run_with({
               config_file: File.join('Project', 'Info.plist'),
@@ -89,7 +91,7 @@ describe BuildAction do
 
     context 'metadata added to args' do
       it "single key:value pair added" do
-        expect(Kernel).to receive(:system).with("#{BUGSNAG_CLI_PATH} create-build --api-key 12345678901234567890123456789DDD --version-name 4.0-project --bundle-version 22 --builder-name josh.edney --revision 3a30a510ba898341ff8631da49dc7021fb28c40e --repository git@github.com:bugsnag/bugsnag-dsym-upload.git --metadata \"test1\": \"First test\"").and_return(true)
+        expect(Kernel).to receive(:system).with("#{BUGSNAG_CLI_PATH} create-build --api-key 12345678901234567890123456789DDD --version-name 4.0-project --bundle-version 22 --builder-name #{BUILDER} --revision #{GIT_REVISION} --repository https://github.com/bugsnag/bugsnag-dsym-upload --metadata \"test1\": \"First test\"").and_return(true)
         Dir.chdir(File.join(FIXTURE_PATH, 'ios_proj')) do
           run_with({
             app_version: '4.0-project',
@@ -100,7 +102,7 @@ describe BuildAction do
       end
 
       it "multiple key:value pairs added" do
-        expect(Kernel).to receive(:system).with("#{BUGSNAG_CLI_PATH} create-build --api-key 12345678901234567890123456789DDD --version-name 4.0-project --bundle-version 22 --builder-name josh.edney --revision 3a30a510ba898341ff8631da49dc7021fb28c40e --repository git@github.com:bugsnag/bugsnag-dsym-upload.git --metadata \"test1\": \"First test\", \"test2\": \"Second test\", \"test3\": \"Third test\"").and_return(true)
+        expect(Kernel).to receive(:system).with("#{BUGSNAG_CLI_PATH} create-build --api-key 12345678901234567890123456789DDD --version-name 4.0-project --bundle-version 22 --builder-name #{BUILDER} --revision #{GIT_REVISION} --repository https://github.com/bugsnag/bugsnag-dsym-upload --metadata \"test1\": \"First test\", \"test2\": \"Second test\", \"test3\": \"Third test\"").and_return(true)
         Dir.chdir(File.join(FIXTURE_PATH, 'ios_proj')) do
           run_with({
                      app_version: '4.0-project',
@@ -111,7 +113,7 @@ describe BuildAction do
       end
 
       it "multiple key:value pairs added as a hash" do
-        expect(Kernel).to receive(:system).with("#{BUGSNAG_CLI_PATH} create-build --api-key 12345678901234567890123456789DDD --version-name 4.0-project --bundle-version 22 --builder-name josh.edney --revision 3a30a510ba898341ff8631da49dc7021fb28c40e --repository git@github.com:bugsnag/bugsnag-dsym-upload.git --metadata \"custom_field_1\"=\"value1\", \"custom_field_2\"=\"value2\"").and_return(true)
+        expect(Kernel).to receive(:system).with("#{BUGSNAG_CLI_PATH} create-build --api-key 12345678901234567890123456789DDD --version-name 4.0-project --bundle-version 22 --builder-name #{BUILDER} --revision #{GIT_REVISION} --repository https://github.com/bugsnag/bugsnag-dsym-upload --metadata \"custom_field_1\"=\"value1\",\"custom_field_2\"=\"value2\"").and_return(true)
         Dir.chdir(File.join(FIXTURE_PATH, 'ios_proj')) do
           run_with({
                      app_version: '4.0-project',
